7afe0aa8bb7b84afdef7bc3e14c5a73c2dea178d
Sebastian <spalacioalzate@gmail.com>
2023-11-30 21:43:49 -0500

    Push Java 17

--- a_proyecto/src/main/java/Proyecto/a_proyecto/FinalCICD.java
+++ a_proyecto/src/main/java/Proyecto/a_proyecto/FinalCICD.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AProyectoApplication {
 
-
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
