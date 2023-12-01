a16ea04438c5960a31e6e13e532ebb1c3ecf59da
Sebastian <spalacioalzate@gmail.com>
2023-11-30 21:55:38 -0500

    Archivo

--- a_proyecto/src/main/java/Proyecto/a_proyecto/FinalCICD.java
+++ a_proyecto/src/main/java/Proyecto/a_proyecto/FinalCICD.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AProyectoApplication {
+public class a_proyecto {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
