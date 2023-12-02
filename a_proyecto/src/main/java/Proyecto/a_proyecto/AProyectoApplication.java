9390c0973e819eef94070d80290b0488e9b0261e
Sebastian <spalacioalzate@gmail.com>
2023-12-02 09:41:05 -0500

    Mala

--- a_proyecto/src/main/java/Proyecto/a_proyecto/AProyectoApplication.java
+++ a_proyecto/src/main/java/Proyecto/a_proyecto/AProyectoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AProyectoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AProyectoApplication.class, args);
+		SpringApplication.rn(AProyectoApplcation.class, args);
 	}
 
 }
